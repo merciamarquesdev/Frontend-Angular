81c8000318de3d0e9f3187090672e83fa8152fe9
Mercia <68623425+mercietc@users.noreply.github.com>
2023-06-26 22:15:59 -0300

    aula1

---
+++ src/script.js
@@ -0,0 +1,17 @@
+console.log("Hello world!");
+console.error("Erro");
+console.warn("Aviso");
+
+idade = 25;
+nome = "Davi";
+//dataNasc = 10/04/1998;
+// comentar: ctrl+;
+// javascript nao é uma linguagem fortemente tipada, nao precisa se preocupar com o tipo, a linguagem infere. sua tipagem é dinamica, ou seja, pode mudar. nao é estatica, que nao muda.
+
+var idade = 10;
+console.log(idade);
+idade = 25;
+console.log(idade);
+
+const naoMuda = 20;
+//naoMuda = 10; //da erro
